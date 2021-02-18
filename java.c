8a3949898ca2b2240d2a6bfe447e654c482e400e
hamdan siddiqi <hamdan@git.train>
2021-02-18 14:02:54 +0530

    adding the info code

---
+++ java.c
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+  System.out.print("Input your first name: ");
+    String fname = input.next();
+  System.out.print("Input your last name: ");
+    String lname = input.next();
+  System.out.println("email=inamsiddiqi95@gmail.com");
+  System.out.println("DoB = 03/10/1996");
+  System.out.println("Mob = 8050564900");
+  System.out.println("Personal info \n"+fname+" "+lname);
+  
+ }
+}
